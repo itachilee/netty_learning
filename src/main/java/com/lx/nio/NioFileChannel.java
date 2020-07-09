34a91e3280d08a6466b92b7bf2ad676ac368c9b1
itachilee <3050155685@qq.com>
2020-07-09 23:50:00 +0800

    NioFileChannel write

--- src/main/java/com/lx/nio/NioFileChannel.java
+++ src/main/java/com/lx/nio/NioFileChannel.java
@@ -1,8 +1,6 @@
 package com.lx.nio;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -11,6 +9,17 @@ import java.nio.channels.FileChannel;
  */
 public class NioFileChannel {
     public static void main(String[] args) throws IOException {
+    // new NioFileChannel(). write();
+    new NioFileChannel().readFromFile();
+    }
+
+    /**
+     * 写入文件
+     * @throws IOException
+     */
+    public void writeToFile() throws IOException {
+
+
         String str ="hello world";
         // 创建一个channel
         FileOutputStream fileOutputStream=new FileOutputStream("D:\\file01.txt");
@@ -28,4 +37,18 @@ public class NioFileChannel {
         fileChannel.write(byteBuffer);
         fileOutputStream.close();
     }
+
+    public void readFromFile() throws IOException {
+        File file=new File("D:\\file01.txt");
+        FileInputStream fileInputStream =new FileInputStream(file);
+        // 通过fileOutStream 获取FileChanel
+        FileChannel fileChannel=fileInputStream.getChannel();
+        // 创建缓冲区
+        ByteBuffer byteBuffer =ByteBuffer.allocate((int)file.length());
+        // 将通道内的数据读入buffer
+        fileChannel.read(byteBuffer);
+        System.out.println(new String(byteBuffer.array()));
+        fileInputStream.close();
+    }
+
 }
