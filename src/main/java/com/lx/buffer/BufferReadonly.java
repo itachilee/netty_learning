bb989d574cfc7e7ceb890a878bcc08f6fa979d19
itachilee <3050155685@qq.com>
2020-07-12 22:14:36 +0800

    readonly buffer'

--- src/main/java/com/lx/buffer/BufferReadonly.java
+++ src/main/java/com/lx/buffer/BufferReadonly.java
@@ -10,5 +10,23 @@ public class BufferReadonly {
     public static void main(String[] args) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(10);
 
+        for (int i = 0; i <byteBuffer.capacity() ; i++) {
+            byteBuffer.put((byte)i);
+        }
+
+        ByteBuffer readOnlyBuffer = byteBuffer.asReadOnlyBuffer();
+        for (int i = 0; i <byteBuffer.capacity() ; i++) {
+            byte b=byteBuffer.get(i);
+            b*=10;
+            byteBuffer.put(i,b);
+        }
+
+        readOnlyBuffer.position(0);
+        readOnlyBuffer.limit(byteBuffer.capacity());
+
+        while(readOnlyBuffer.remaining()>0){
+            System.out.println(readOnlyBuffer.get());
+        }
+
     }
 }
